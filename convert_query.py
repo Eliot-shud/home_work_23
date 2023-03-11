33d978dc0506b13171bde13acf1a32a907ad39ca
Eliot-shud <elyarkamol1630@gmail.com>
2023-03-11 11:58:40 +0700

    initial commit

--- convert_query.py
+++ convert_query.py
@@ -1,27 +1,30 @@
 from typing import Optional
-
-from functions import filter_query,  limit_query, map_query, sort_query, unique_query
-
+from functions import filter_query, limit_query, map_query, sort_query, unique_query
+from typing import Iterable
 
 CMD_TO_FUNCTIONS = {
     'filter': filter_query,
     'limit': limit_query,
     'map': map_query,
     'sort': sort_query,
-    'unique': unique_query,
+    'unique': unique_query
 }
 
 
-def read_file(file_name: str):
+def read_file(file_name):
     with open(file_name) as file:
         for line in file:
             yield line
 
 
-def build_query(cmd: str, value: str, file_name: str, data: Optional[list[str]]):
-    if data is None:
-        prepared_data = read_file(file_name)
-    else:
-        prepared_data = data
+def build_query(cmd, value, file_name, data: Optional[Iterable[str]]):
 
-    return list(CMD_TO_FUNCTIONS[cmd](value=value, data=prepared_data))
+    if data is None:
+        new_data = read_file(file_name)
+    else:
+        new_data = data
+    return list(CMD_TO_FUNCTIONS[cmd](value=value, data=new_data))
+
+
+def get_resault(cmd, value, file_name, data):
+    return build_query(cmd, value, file_name, data)
--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
-from convert_query import build_query
+from convert_query import build_query, get_resault
 from models import RequestSchema
 
 main_bp = Blueprint('main', __name__)
@@ -10,19 +10,21 @@ FILE_NAME = 'data/apache_logs.txt'
 
 @main_bp.route('/perform_query', methods=['POST'])
 def perform_query():
+
     data = request.json
     try:
-        validated_data = RequestSchema().load(data)
-    except ValidationError as e:
-        return jsonify(e.messages), 400
-    result = None
+        check_data = RequestSchema().load(data)
+    except ValidationError as error:
+        return jsonify(error.messages), 400
 
-    for query in validated_data['queries']:
-        result = build_query(
-            cmd=query['cmd'],
-            value=query['value'],
-            file_name=FILE_NAME,
-            data=result,
-        )
+    cmd = check_data.get('cmd1')
+    value = check_data.get('value1')
+    file_name = check_data.get('file_name')
+    data = get_resault(cmd, value, file_name, None)
+
+    cmd = check_data.get('cmd2')
+    value = check_data.get('value2')
+    file_name = check_data.get('file_name')
+    result = get_resault(cmd, value, file_name, data)
 
     return jsonify(result)
